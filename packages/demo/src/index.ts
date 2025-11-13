9894ddcd6b12380cdef25f4525df9d11a3848cd1
toxeeec <bartosz.kapciak@gmail.com>
2025-11-13 18:18:13 +0100

    feat: initial api

--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -1,6 +1,7 @@
-import { add } from "minireact";
+import { createRoot, jsx } from "minireact";
 
-const res = add(1, 2);
-console.log(res);
+createRoot(document.getElementById("root")!).render(
+  jsx("div", { children: "Hello world" }),
+);
 
 new EventSource("/esbuild").addEventListener("change", () => location.reload());
--- packages/minireact/src/index.ts
+++ packages/minireact/src/index.ts
@@ -1,3 +1,28 @@
-export function add(a: number, b: number) {
-  return a + b;
+type Container = Element | Document | DocumentFragment;
+
+type FiberRoot = {
+  container: Container;
+};
+
+type ReactElement = {
+  type: string;
+  props: object;
+};
+
+class ReactDOMRoot {
+  #internalRoot: FiberRoot;
+
+  constructor(internalRoot: FiberRoot) {
+    this.#internalRoot = internalRoot;
+  }
+
+  render(children: ReactElement) {}
+}
+
+export function createRoot(container: Container): ReactDOMRoot {
+  return new ReactDOMRoot({ container });
+}
+
+export function jsx(type: string, props: object): ReactElement {
+  return { type, props };
 }
